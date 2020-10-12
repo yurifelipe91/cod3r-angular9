ce0a89ba355d7ecc10816e0bc920a201c254c7b1
yurifelipe91 <66404148+yurifelipe91@users.noreply.github.com>
2020-10-12 20:40:47 -0300

    add novos imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,8 +9,8 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import { HomeComponent } from './views/home/home.component';
-// import { RedDirective } from './directives/red.directive';
-// import { ForDirective } from './directives/for.directive';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
@@ -25,7 +25,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -35,10 +45,13 @@ import { MatInputModule } from '@angular/material/input';
     FooterComponent,
     NavComponent,
     HomeComponent,
-    // RedDirective,
-    // ForDirective,
+    RedDirective,
+    ForDirective,
     ProductCreateComponent,
     ProductCrudComponent,
+    ProductReadComponent,
+    ProductRead2Component,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
@@ -53,9 +66,15 @@ import { MatInputModule } from '@angular/material/input';
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
